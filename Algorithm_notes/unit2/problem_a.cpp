26f4def8049ebdb7cc7278ce0d32df128444b64f
Junjun Li <junjunli666@gmail.com>
2019-01-20 12:03:50 +0800

    add problem a

---
+++ Algorithm_notes/unit2/problem_a.cpp
@@ -0,0 +1,13 @@
+/*
+题目来源：http://codeup.cn/problem.php?cid=100000566&pid=0
+题目描述
+编写一个C程序，要求在屏幕上输出一下一行信息。
+This is my first c program!
+*/
+#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+    printf("This is my first c program!");
+    return 0;
+}
