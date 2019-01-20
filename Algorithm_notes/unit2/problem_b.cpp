1f1d11dc78c78d2bd74d73c723658b5ac1dd4f4e
Junjun Li <junjunli666@gmail.com>
2019-01-20 12:30:54 +0800

    add  problem program

---
+++ Algorithm_notes/unit2/problem_b.cpp
@@ -0,0 +1,19 @@
+/*
+题目来源：http://codeup.cn/problem.php?cid=100000566&pid=1
+题目描述
+编写一个C语言程序，输出以下信息：
+
+********************
+
+Very Good!
+
+********************
+*/
+#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+    printf("******************** \n");
+    printf("Very Good! \n");
+    printf("******************** ");
+}
---
+++ Algorithm_notes/unit2/problem_c.cpp
@@ -0,0 +1,14 @@
+// http://codeup.cn/problem.php?cid=100000566&pid=2
+// 题目描述
+// 例题1-2-1 求两个整数之和
+// 设置3个变量a, b, sum，其中a, b用来存放两个整数，sum用来存放a, b两个数的和，
+// 通过赋值（即采用赋值运算符"="）的方式将a初始化为123，b初始化为456，并把两个变量相加的结果赋值给sum。
+
+# include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+    int a = 123, b = 456;
+    printf("sum=%d", a + b);
+    return 0;
+}
