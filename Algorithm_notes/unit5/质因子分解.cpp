ca0647c3f55926cb7ae2ba636b65371d48184dc4
Junjun Li <junjunli666@gmail.com>
2019-02-02 15:21:38 +0800

    质因子分解 补充

--- Algorithm_notes/unit5/质因子分解.cpp
+++ Algorithm_notes/unit5/质因子分解.cpp
@@ -31,16 +31,42 @@ void generate_prime_table() {
 	}
 }
 
+struct factor
+{
+	int x;  // 某个质数
+	int cnt = 0;   // 该质数出现的次数
+}fac[10];
+
 int main(int argc, char const *argv[])
 {
     generate_prime_table();
 
-    int n;
+    int n, fac_n = 0;   // fac_n 为第n个质因子
     scanf("%d", &n);
-    if (n == 1) {
-    	printf("1=1");
-    }
     
+    if(n == 1) {
+        printf("1=1\n");
+    } else {
+        // todo next things. 求数字的因子
+        int sqr = (int)sqrt(n);
+
+        // 枚举sqrt(n)以内的质因子
+        for (int i = 0; i < prime_count && prime_table[i] <= sqrt(n); ++i)
+        {
+        	if (n % prime_table[i] == 0)
+        	{
+        		fac[fac_n].x = prime_count[i];
+        		
+        		// 记录该质因子出现的个数
+        		while(n % prime_table[i] == 0) {
+        			n = n / prime_table[i];
+        			fac[fac_n].cnt ++;
+        		}
+        		fac_n++;
+        	}
+
+        }
+    }
 
 	for (int i = 0; i < prime_count; ++i)
 	{
@@ -48,3 +74,7 @@ int main(int argc, char const *argv[])
 	}
 	return 0;
 }
+
+/*
+这道题目有些复杂，暂时还没有搞懂...
+*/
