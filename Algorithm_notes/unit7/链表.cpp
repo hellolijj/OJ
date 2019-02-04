6eb38f66aa2c172aabffb516c68c9936683b8733
Junjun Li <junjunli666@gmail.com>
2019-02-04 19:55:02 +0800

    链表元素的删除

--- Algorithm_notes/unit7/链表.cpp
+++ Algorithm_notes/unit7/链表.cpp
@@ -38,14 +38,75 @@ void print(Node *head) {
 
 }
 
+// 在链表中查找，统计x元素的个数
+int count(Node *head, int x) {
+
+	int cnt = 0;
+
+	Node *L = head->next;
+
+	while(L != NULL) {
+		if(L->data == x) {
+			cnt ++;
+		}
+		L = L->next;
+	}
+
+	return cnt;
+}
+
+// 在pos处插入一个元素
+void insert(Node *head, int pos, int x) {
+
+	Node *L = head;
+
+	for(int i = 0; i < pos; i++) {
+		L = L->next;
+	}
+	// 创建一个节点
+	Node *p = new Node;
+	p->data = x;
+
+	// 链接节点
+	p->next = L->next;
+	L->next = p;
+}
+
+// 删除元素
+void del(Node *head, int x) {
+
+	Node *L = head->next;
+	Node *pre = head;     // pre始终保持p的前驱节点
+
+	while(L != NULL) {
+		if(L->data == x) {
+			pre->next = L->next;
+			delete(L);
+			L = pre->next;
+		} else {
+			L = L->next;
+			pre = pre->next;
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	int a[10] = {2, 3, 4, 5, 6, 8, 9, 0, 1, 7};
+	int a[10] = {2, 3, 4, 4, 6, 4, 9, 0, 1, 7};
 	
-	print(create(a, 10));
+	Node *head = create(a, 10);
+	print(head);
+
+	printf("元素4的个数: %d\n",count(head, 4));
+
+	insert(head, 2, 100);
+	print(head);
+
+	del(head, 100);
+	print(head);
 	
 	return 0;
 }
 
 // 总结：
-// 没创建一个节点，节点的 p->next = NULL
+// 每y创建一个节点，节点的 p->next = NULL
