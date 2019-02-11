99bdf5dbf0f469a60a80f9c5cd383f8e43a42c3f
Junjun Li <junjunli666@gmail.com>
2019-02-11 11:43:01 +0800

    微博广度优先遍历问题

--- Algorithm_notes/图/广度优先搜素_微博问题.cpp
+++ Algorithm_notes/图/广度优先搜素_微博问题.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -15,12 +16,52 @@ vector<Node> Adj[N];  // 使用邻接表来构造图
 
 bool inq[N] = {false};     //来记录图中节点是否已经访问
 
+// bfs 遍历 s 表示起点， l是遍历的层数
+int BFS(int s, int l) {
+
+	int forward_cnt = 0;
+
+	queue<Node> q;
+	
+	Node start;
+	start.id = s;
+	start.layer = 0;
+
+	q.push(start);
+	inq[s] = true;
+
+	while(!q.empty()) {
+		Node top = q.front();
+		q.pop();
+
+		int u = top.id;
+
+		// 遍历邻接元素
+		for (int i = 0; i < Adj[u].size(); ++i)
+		{
+			Node next = Adj[u][i];  // 取出这个元素的follower
+			next.layer = top.layer + 1;
+
+			if (inq[next.id] == false && next.layer <= l)
+			{
+				q.push(next);
+				inq[next.id] = true;
+				// printf("%d ", next.id);
+				forward_cnt++;
+			}
+		}
+	}
+
+	printf("\n");
+	return forward_cnt;
+}
 
 int main(int argc, char const *argv[])
 {
-	int n;
-	scanf("%d", &n);
-
+	int n, l;
+	scanf("%d%d", &n, &l);
+	
+	
 	// 输入每一个用户的follower
 	for (int i = 1; i <= n; ++i)
 	{
@@ -30,12 +71,21 @@ int main(int argc, char const *argv[])
 		int follower_cnt;
 		scanf("%d", &follower_cnt);
 
-		for (int j = 1; i <= follower_cnt; ++i)
+		for (int j = 1; j <= follower_cnt; ++j)
 		{
 			int follower_id;
 			scanf("%d", &follower_id);
 			Adj[follower_id].push_back(user);
 		}
+	}
+
+	int query_times;
+	scanf("%d", &query_times);
+	for(int i = 0; i < query_times; i++) {
+		int query_id;
+		scanf("%d", &query_id);
+
+		printf("%d\n", BFS(query_id, l));
 	}
 
 	return 0;
