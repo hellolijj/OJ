8eb139db194f1de6b4e9b67efd169ad91653b714
Junjun Li <junjunli666@gmail.com>
2019-02-17 15:39:30 +0800

    有序二维数组查找

---
+++ nowcoder/数组/二维数组的查找.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <vector>
+
+
+using namespace std;
+
+/* 思路
+
+因为是有序二维数组，从左下角往右上方找过去
+
+*/
+
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        for(int i = array.size() - 1, j = 0; i >= 0 &&j < array[0].size(); ) {
+            if(target == array[i][j]) {
+                return true;
+            }
+            if(array[i][j] < target) {
+                j++;
+                continue;
+            }
+            if(array[i][j] > target) {
+                i--;
+                continue;
+            } 
+        }
+        return false;
+    }
+};
+
+int main() {
+    
+    vector<vector<int> >vi(2, vector<int>(3));
+
+    int a[2][3] = {{2, 3, 4}, {6, 9, 10}};
+
+    for(int i = 0; i < vi.size(); i++) {
+        for(int j = 0; j < vi[i].size(); j++) {
+            vi[i][j] = a[i][j];
+        }
+    }
+    
+    class Solution Solu;
+    
+	if(Solu.Find(10, vi)) {
+        printf("查到了\n");
+    } else {
+        printf("没有查到\n");
+    }
+    return 0;
+}
+
+/*
+注意点：
+1、vector的基本使用，一维数组、二维数组
+2、这个题目中有序二维数组的查找，时间复杂度点优化
+*/
