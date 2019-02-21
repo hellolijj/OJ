ac45e553830b8554ce1ffa8c64cbe02e4b125d81
Junjun Li <junjunli666@gmail.com>
2019-02-21 16:06:58 +0800

    vector 构造时 左开右闭

--- c++/vector/vector_构造.cpp
+++ c++/vector/vector_构造.cpp
@@ -9,7 +9,8 @@ int main() {
     vector<int> vi(5, 1939);
     
     // vector<int> obj(vi)
-    vector<int> obj(vi.begin()+2, vi.end());
+    // vector<int> obj(vi.begin()+2, vi.end());
+    vector<int> obj(vi.begin(), vi.end());  // [） begin相当于0 end() 相当于5
 
     for(int i = 0; i < vi.size(); i++) {
         printf("%d ", vi[i]);
