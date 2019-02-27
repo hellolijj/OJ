eb8997e18b8706cceada6ea6b744a0a7f7e0526c
Junjun Li <junjunli666@gmail.com>
2019-02-27 11:22:59 +0800

    旋转的数组二分法实现

--- nowcoder/数组/旋转的数组.cpp
+++ nowcoder/数组/旋转的数组.cpp
@@ -8,33 +8,58 @@ using namespace std;
 
 class Solution {
 public:
+    int minNumberInRotateArray1(vector<int> rotateArray) {
+        
+        if(rotateArray.size() == 0) {
+            return 0;
+        }
+        
+        for(int i = 0; i < rotateArray.size()-1; i++) {
+            if(rotateArray[i] > rotateArray[i+1]) {
+                return rotateArray[i+1];
+            }
+        }
+        
+        return rotateArray[0];
+    }
     int minNumberInRotateArray(vector<int> rotateArray) {
         int length = rotateArray.size();
         if(length == 0) {
-            return -1;
+            return 0;
         }
 
         int low = 0, hign = length-1;
-        while(low < hign) {
-            int min = low + (hign - low) / 2;
-            if(rotateArray[min] < rotateArray[low]) {
+        int min = 0;
+
+        while(rotateArray[low] > rotateArray[hign]) {
+            if(hign - low == 1) {
+                min = hign;
+                break;
+            }
+            min = low + (hign - low) / 2;
+            if (rotateArray[min] >= rotateArray[low]) {
                 low = min;
             }
-            if(rotateArray[min] < rotateArray[hign]) {
-                hign = min+1;
+            if (rotateArray[min] <= rotateArray[hign]) {
+                hign = min;
             }
         }
-        return rotateArray[low];
+
+        return rotateArray[min];
     }
+
 };
 
 int main() {
     int a[5] = {4,5,6,1,2};
-    vector<int> v(5);
-    for(int i = 0; i < v.size(); i++) {
+    vector<int> v;
+    for(int i = 0; i < 5; i++) {
         v.push_back(a[i]);
     } 
     class Solution solu;
     printf("%d\n", solu.minNumberInRotateArray(v));
+    printf("%d\n", solu.minNumberInRotateArray1(v));
     return 0;
 }
+
+// 使用二分法不确定 a[min] 与 谁比较然后作出怎么样的操作
