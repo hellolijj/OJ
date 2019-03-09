0fc92fea0257d83249b7331c5ddfdfaeb86130ac
Junjun Li <junjunli666@gmail.com>
2019-03-09 19:03:25 +0800

    腾讯2017暑期 最大公共子字符串问题

--- 公司上机/腾讯2017暑期/01.cpp
+++ 公司上机/腾讯2017暑期/01.cpp
@@ -5,33 +5,29 @@
 
 using namespace std;
 
-const int N = 101;
-
 int max_length(string s1, string s2) {
     int len1 = s1.length();
     int len2 = s2.length();
-    int res = 0;  // 记录最大的公共字串长度
     vector<vector<int> > v(len1+1, vector<int>(len2+1));
     for(int i = 0; i <= len1; i++) {
         for(int j = 0; j <= len2; j++) {
             if(i == 0 || j == 0) {
                 v[i][j] = 0;
             } else if(s1[i-1] == s2[j-1]) {
-                v[i][j] = v[i-1][j-1] + 1;
-                res = max(v[i][j], res);  
+                v[i][j] = v[i-1][j-1] + 1;  
             } else {
-                v[i][j] = 0;
+                v[i][j] = max(v[i][j-1], v[i-1][j]);
             }
         }
     }
-    return res;
+    return v[len1][len2];
 }
 
 int main() {
-
-    string str1 = "sdfagdasgasdgas";
-    string str2 = "sdfadgdasgasd";
-
-    cout << max_length(str1, str2) << endl;
+    string s;
+    while(cin >> s) {
+        string r(s.rbegin(), s.rend());
+        cout << s.length() - max_length(s, r) << endl;
+    }
     return 0;
 }
